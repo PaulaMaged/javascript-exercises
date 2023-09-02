e8bbccb0b2adec3ca3ad0d5b728aaab83bf96cbb
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2023-09-02 13:52:02 +0300

    Solved exercises 1 through 7 inclusive!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...filterOut) {
+    for(const filterItem of filterOut) {
+        index = array.indexOf(filterItem);
+        while(index > -1) {
+            array.splice(index, 1);
+            index = array.indexOf(filterItem);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(limit1, limit2) {
+    for(const input of arguments) 
+        if(typeof input !== 'number') return 'ERROR';
 
+    const start = Math.min(...arguments);
+
+    if(start < 0) return 'ERROR';
+
+    const end = Math.max(...arguments);
+    let sum = 0;
+
+    for(let i = start; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
