f5e968b44eedf2066530f1beaceb0fa7daadef1b
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2023-09-10 14:41:12 +0300

    Finished calculator exercise and checked solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfInts) {
+	return arrayOfInts.reduce((total, member) => {
+    return total + member;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfInts) {
+  if(!arrayOfInts.length) return -1;
+  return arrayOfInts.reduce((total, member) => {
+    return total * member;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	// return base**exponent;
+  if(exponent == 1) return base;
+  if(exponent == 0) return 1;
+  const arrayOfBase = [];
+  while(exponent > 0){
+    arrayOfBase.push(base);
+    exponent--;
+  }
+  return multiply(arrayOfBase);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(!number) return 1;
+  let factorial = 1;
+  while(number > 1){
+    factorial *= number;
+    number--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
