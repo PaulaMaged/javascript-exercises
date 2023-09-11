0e25a2cef3620180321622e031a7fe0b328a8fc8
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2023-09-11 13:25:14 +0300

    Exercise 9 & 10, phew

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const puncArray = ['!', ',', ' ', '.', "'", '"','?'];
+    const lowerCaseStringArray = string.toLowerCase().split('');
+    const punctFilter = lowerCaseStringArray.filter(element => !puncArray.includes(element));
+    const reverseString = [...punctFilter].reverse();
+    return punctFilter.join() === reverseString.join();
 };
 
+// console.log(palindromes('rac,?!""!?,car'));
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n == 1)
+        return 1;
+    else if(n < 1)
+        return "OOPS";
+    const fibSeries = [1,1];
+    let counter = 0;
+    let result;
+    while(fibSeries.length != n) {
+        const [num1, num2] = fibSeries.slice(counter, fibSeries.length);
+        result = num1 + num2;
+        fibSeries.push(result);
+        counter++;
+    }
+    return fibSeries[fibSeries.length - 1];
 };
 
+console.log(fibonacci(0));
 // Do not edit below this line
 module.exports = fibonacci;
